2e8f74ff980bab6b2c1cd22dc96e26e535b1c675
Michael Huang <michaelyhuang23@gmail.com>
2021-11-04 10:57:40 -0400

    main gen model complete

--- model_zoo/auxiliary_layers.py
+++ model_zoo/auxiliary_layers.py
@@ -6,16 +6,17 @@ class SentenceAgg(nn.Module):
 	def __init__(self):
 		super().__init__()
 		self.linear1 = nn.Linear(768, 1536)
+		self.relu = nn.ReLU()
 		self.linear2 = nn.Linear(1536, 200)
 	
-	def __call__(self, x):
+	def forward(self, x):
 		'''
 		inputs: (B, T, D) embeddings for the words
 		obtain sentence embedding
 		'''
-		x = self.linear1(x)
+		x = self.relu(self.linear1(x))
 		x = torch.mean(x, dim = -2)
-		x = self.linear2(x)
+		x = self.relu(self.linear2(x))
 		return x
 
 class CondAug(nn.Module):
@@ -26,7 +27,7 @@ class CondAug(nn.Module):
 		self.std_weight = torch.normal(1, 0.5)
 		self.std_weight.requires_grad = True
 
-	def __call__(self, x):
+	def forward(self, x):
 		return x # implement later
 
 class Image2TextAttention(nn.Module):
@@ -35,7 +36,7 @@ class Image2TextAttention(nn.Module):
 		assert(output_shape%num_head == 0)
 		self.attentor = nn.MultiheadAttention(output_shape, num_head, batch_first=True)
 
-	def __call__(self, img, txt):
+	def forward(self, img, txt):
 		'''
 		img = (N, Di, H, W)
 		txt = (N, T, Dt)
@@ -50,7 +51,12 @@ class Image2TextAttention(nn.Module):
 		return attn_output_weights
 
 
+class ImgGen(nn.Module):
+	def __init__(self, input_shape):
+		super().__init__()
+		self.conv1 = nn.Conv2d(input_shape, 3, (3,3), padding=1)
+		self.sigmoid = nn.Sigmoid()
 
-
-
+	def forward(self, x):
+		return self.sigmoid(self.conv1(x))
 
--- model_zoo/generative_model.py
+++ model_zoo/generative_model.py
@@ -1,71 +1,34 @@
 import torch
 from torch import nn
 
-class FirstGen(nn.Module):
+from backbone_bert import BERTBackbone
+from auxiliary_layers import SentenceAgg, CondAug, Image2TextAttention, ImgGen
+from generative_layers import FirstGen, SecondGen, ThirdGen
+
+class AttnGAN(nn.Module):
 	def __init__(self):
-		super().__init__()
-		self.linear1 = nn.Linear(200, 18432)
-		self.convup1 = nn.ConvTranspose2d(512, 256, (4,4), stride=1, padding=0)
-		self.convup2 = nn.ConvTranspose2d(256, 128, (4,4), stride=2, padding=0)
-		self.convup3 = nn.ConvTranspose2d(128, 64, (7,7), stride=3, padding=0)
+		self.backbone  = BERTBackbone()
+		self.sentence_alg = SentenceAgg()
+		self.cond_aug = CondAug()
+		self.first_part = FirstGen()
+		self.attn_1 = Image2TextAttention(64)
+		self.second_part = SecondGen()
+		self.attn_2 = Image2TextAttention(64)
+		self.third_part = ThirdGen()
+		pass
 
-	def __call__(self, x):
-		'''
-		x is of shape (B, D)
-		output is of shape (B, 64, 64, 64)
-		'''
-		x = self.linear1(x)
-		x = torch.reshape(x.shape[0], 512, 6, 6)
-		x = self.convup1(x)
-		x = self.convup2(x)
-		x = self.convup3(x)
-		return x
-
-class ResBlock(nn.Module):
-	def __init__(self):
-		super().__init__()
-		self.conv1 = nn.Conv2d(64, 128, (3,3), 1, padding=1)
-		self.batchnorm1 = nn.BatchNorm2d(128, eps=1e-05, momentum=0.1, affine=True)
-		self.relu = nn.ReLU()
-		self.conv2 = nn.Conv2d(128, 64, (3,3), 1, padding=1)
-		self.batchnorm2 = nn.BatchNorm2d(64, eps=1e-05, momentum=0.1, affine=True)
-	def __call__(self, inputs):
-		x = self.relu(self.conv1(inputs))
-		x = self.batchnorm1(x)
-		x = self.relu(self.conv2(x))
-		x = self.batchnorm2(x+inputs)
-		return x
-
-class SecondGen(nn.Module):
-	def __init__(self):
-		super().__init__()
-		self.res1 = ResBlock()
-		self.res2 = ResBlock()
-		self.convup1 = nn.ConvTranspose2d(64, 64, (4,4), stride=2, padding=1)
-
-	def __call__(self, x):
-		'''
-		x is of shape (B, D)
-		output is of shape (B, 64, 128, 128)
-		'''
-		x = self.res1(x)
-		x = self.res2(x)
-		x = self.convup1(x)
-		return x
-
-class ThirdGen(nn.Module):
-	def __init__(self):
-		super().__init__()
-		self.res1 = ResBlock()
-		self.res2 = ResBlock()
-		self.convup1 = nn.ConvTranspose2d(64, 64, (4,4), stride=2, padding=1)
-
-	def __call__(self, x):
-		'''
-		x is of shape (B, D)
-		output is of shape (B, 64, 256, 256)
-		'''
-		x = self.res1(x)
-		x = self.res2(x)
-		x = self.convup1(x)
-		return x
+	def foward(self, x):
+		word_fs = self.backbone(x)
+		sentence_f = self.sentence_alg(word_fs)
+		sentence_f = self.cond_aug(sentence_f)
+		first_stage = self.first_part(sentence_f)
+		first_img = ImgGen(first_stage)
+		second_stage = self.attn_1(first_stage, word_fs) + first_stage
+		# add weighing constant
+		second_stage = self.second_part(second_stage)
+		second_img = ImgGen(second_stage)
+		third_stage = self.attn_1(second_stage, word_fs) + second_stage
+		third_stage = self.third_part(third_stage)
+		third_img = ImgGen(third_stage)
+		return first_img, second_img, third_img
+		
